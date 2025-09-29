7dc1cebefb30da241a9e276a5b43cb3cadcffae5
mishakolomiiets <59320513+mishakolomiiets@users.noreply.github.com>
2025-09-29 22:08:46 +0300

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,6 +4,10 @@ export function Footer() {
       <p className="text-white text-sm py-4 lg:py-8" dir="ltr">
         ©2025 Next college. All right Reserved.
       </p>
+      <p>
+      <a href="https://lp.nextcollege.co.il/privacy-policy/" target="_blank">מדיניות פרטיות - נקסט קולג'
+</a>
+      </p>
     </div>
   );
 }
